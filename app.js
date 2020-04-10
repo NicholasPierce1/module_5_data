6561cb4e3e3630054927c9ca62cdedcc56a39eb4
NicholasPierce1 <42748558+NicholasPierce1@users.noreply.github.com>
2020-04-09 19:29:13 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+
+const mongodb = require("mongoose");
+// second nick is pwd
+const db_connect = "mongodb+srv://nick:nick@cluster0-f6vlf.mongodb.net/library?retryWrites=true&w=majority";
+mongodb.connect(db_connect, {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongodb.connection;
+db.on("error", console.error.bind(console, 'could not connect to mongo'));
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+
+app.listen(3002, ()=>{console.log("running now.");});
